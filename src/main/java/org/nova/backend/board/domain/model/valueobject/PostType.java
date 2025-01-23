fbca111deb50e0ae4a494d3ef21bb3bf17f2ef57
SinnoLn <sintmtn69@gmail.com>
2025-01-23 23:24:25 +0900

    feat: PostType enum 추가

---
+++ src/main/java/org/nova/backend/board/domain/model/valueobject/PostType.java
@@ -0,0 +1,11 @@
+package org.nova.backend.board.domain.model.valueobject;
+
+public enum PostType {
+    QNA,
+    FREE,
+    INTRODUCTION,
+    NOTICE,
+    EXAM_ARCHIVE,
+    PICTURES,
+    SUGGESTION
+}
