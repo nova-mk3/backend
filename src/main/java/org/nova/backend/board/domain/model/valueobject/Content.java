1adc48c6c7e38994b6413e0fad49aa63c5b0b315
SinnoLn <sintmtn69@gmail.com>
2025-01-24 09:44:53 +0900

    Feat: 게시글 및 댓글 내용을 관리하기 위한 Content 값 객체 추가
    - 내용 필드의 null, 빈 값, 길이 제한(5000자) 검증 로직 포함

--- src/main/java/org/nova/backend/board/domain/model/valueobject/Content.java
+++ src/main/java/org/nova/backend/board/domain/model/valueobject/Content.java
@@ -1,26 +1,25 @@
 package org.nova.backend.board.domain.model.valueobject;
 
 import jakarta.persistence.Embeddable;
+import lombok.Getter;
 
+@Getter
 @Embeddable
 public final class Content {
-    private final String value;
+    private final String content;
 
     protected Content(){
-        this.value = "";
+        this.content = "";
     }
 
-    public Content(String value){
-        if (value == null || value.trim().isEmpty()) {
+    public Content(String content){
+        if (content == null || content.trim().isEmpty()) {
             throw new IllegalArgumentException("내용은 비어 있을 수 없습니다.");
         }
-        if (value.length() > 5000) {
+        if (content.length() > 5000) {
             throw new IllegalArgumentException("내용은 5000자를 초과할 수 없습니다.");
         }
-        this.value = value;
+        this.content = content;
     }
 
-    public String getValue() {
-        return value;
-    }
 }
