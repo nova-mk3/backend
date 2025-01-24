2530de29097399fb0b7cd8abea7a55ec9a27f6c2
SinnoLn <sintmtn69@gmail.com>
2025-01-24 09:44:45 +0900

    Feat: 댓글 관리를 위한 Comment 엔티티 추가
    - 게시글과 부모 댓글을 참조하는 관계 설정
    - 댓글 내용(Content)과 생성/수정 시간 필드 추가

--- src/main/java/org/nova/backend/board/domain/model/entity/Comment.java
+++ src/main/java/org/nova/backend/board/domain/model/entity/Comment.java
@@ -8,8 +8,15 @@ import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 import java.time.LocalDateTime;
 import java.util.UUID;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.nova.backend.board.domain.model.valueobject.Content;
 
 @Entity
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
 @Table(name = "comment")
 public class Comment {
     @Id
@@ -23,7 +30,7 @@ public class Comment {
     @JoinColumn(name = "parent_comment_id")
     private Comment parentComment;
 
-    private String content;
+    private Content content;
 
     private LocalDateTime create_time;
 
