9dfc12ab17232552f514bd5dce409547d78dcbb2
SinnoLn <sintmtn69@gmail.com>
2025-01-28 18:41:17 +0900

    feat: 공통응답코드 203, 201추가

--- src/main/java/org/nova/backend/shared/model/ApiResponse.java
+++ src/main/java/org/nova/backend/shared/model/ApiResponse.java
@@ -2,6 +2,7 @@ package org.nova.backend.shared.model;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import org.springframework.http.HttpStatus;
 
 
 @Data
@@ -12,7 +13,15 @@ public class ApiResponse<T> {
     private T data;
 
     public static <T> ApiResponse<T> success(T data) {
-        return new ApiResponse<>(200, "Success", data);
+        return new ApiResponse<>(HttpStatus.OK.value(), "success", data);
+    }
+
+    public static <T> ApiResponse<T> created(T data) {
+        return new ApiResponse<>(HttpStatus.CREATED.value(), "success created", data);
+    }
+
+    public static ApiResponse<Void> noContent() {
+        return new ApiResponse<>(HttpStatus.NO_CONTENT.value(), "success noContent", null);
     }
 
     public static ApiResponse<Void> error(String message, int status) {
