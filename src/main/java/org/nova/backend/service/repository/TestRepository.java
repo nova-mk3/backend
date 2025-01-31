404396d1401d34a689fda2a14c96acb274c823fe
SinnoLn <sintmtn69@gmail.com>
2025-01-31 10:40:17 +0900

    Refactor: 안쓰는 파일 삭제

--- src/main/java/org/nova/backend/service/repository/TestRepository.java
+++
@@ -1,7 +0,0 @@
-package org.nova.backend.service.repository;
-
-import org.nova.backend.service.entity.Test;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface TestRepository extends JpaRepository<Test, Long>, TestRepositoryCustom {
-}
