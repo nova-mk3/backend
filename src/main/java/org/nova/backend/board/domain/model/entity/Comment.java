7139ba509cb01896cf0fac4f161a42b405528589
SinnoLn <sintmtn69@gmail.com>
2025-02-01 01:27:07 +0900

    Refactor: Comment 엔티티 UUID 자동으로 받게 수정

--- src/main/java/org/nova/backend/board/domain/model/entity/Comment.java
+++ src/main/java/org/nova/backend/board/domain/model/entity/Comment.java
@@ -1,9 +1,10 @@
 package org.nova.backend.board.domain.model.entity;
 
-import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
@@ -22,6 +23,7 @@ import org.nova.backend.member.domain.model.entity.Member;
 @Table(name = "comment")
 public class Comment {
     @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
     @Column
     private UUID id;
 
