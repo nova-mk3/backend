ce15809d116cd753e21cf71808a6df017fae960b
SinnoLn <sintmtn69@gmail.com>
2025-02-01 01:27:36 +0900

    Refactor: CommentRequest 생성

---
+++ src/main/java/org/nova/backend/board/application/dto/request/CommentRequest.java
@@ -0,0 +1,17 @@
+package org.nova.backend.board.application.dto.request;
+
+import jakarta.validation.constraints.NotBlank;
+import java.util.UUID;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class CommentRequest {
+    private UUID postId;
+    private UUID parentCommentId;
+    @NotBlank(message = "댓글 내용은 비어 있을 수 없습니다.")
+    private String content;
+}
