44e50c2e7fe8afa8c7e4d765bb5b3e9ed81ae286
SinnoLn <sintmtn69@gmail.com>
2025-02-01 16:55:33 +0900

    Feat: 게시글 업데이트 usecase

--- src/main/java/org/nova/backend/board/application/port/in/PostUseCase.java
+++ src/main/java/org/nova/backend/board/application/port/in/PostUseCase.java
@@ -3,6 +3,7 @@ package org.nova.backend.board.application.port.in;
 import java.util.List;
 import java.util.UUID;
 import org.nova.backend.board.application.dto.request.BasePostRequest;
+import org.nova.backend.board.application.dto.request.UpdatePostRequest;
 import org.nova.backend.board.application.dto.response.PostResponse;
 import org.nova.backend.board.domain.model.valueobject.BoardCategory;
 import org.nova.backend.member.domain.model.entity.Member;
@@ -14,6 +15,7 @@ public interface PostUseCase {
     PostResponse createPost(BasePostRequest request, Member member, List<MultipartFile> files);
     Page<PostResponse> getPostsByCategory(BoardCategory category, Pageable pageable);
     PostResponse getPostById(UUID postId);
+    PostResponse updatePost(UUID postId, UpdatePostRequest request, Member member, List<MultipartFile> files);
 
     int likePost(UUID postId, Member member);
     int unlikePost(UUID postId, Member member);
