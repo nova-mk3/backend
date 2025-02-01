b7c8a474ab44057f81150a342789f897a19e155e
SinnoLn <sintmtn69@gmail.com>
2025-02-01 16:55:47 +0900

    Feat: 특정 파일 삭제 api

--- src/main/java/org/nova/backend/board/application/port/in/FileUseCase.java
+++ src/main/java/org/nova/backend/board/application/port/in/FileUseCase.java
@@ -1,11 +1,13 @@
 package org.nova.backend.board.application.port.in;
 
 import java.util.List;
+import java.util.UUID;
 import org.nova.backend.board.domain.model.entity.File;
 import org.nova.backend.board.domain.model.entity.Post;
 import org.springframework.web.multipart.MultipartFile;
 
 public interface FileUseCase {
     List<File> saveFiles(List<MultipartFile> files, Post post);
+    void deleteFiles(List<UUID> fileIds);
 }
 
