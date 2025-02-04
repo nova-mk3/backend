5c33293a924cd571b401fe6907b8eacace2c3cca
SinnoLn <sintmtn69@gmail.com>
2025-02-04 10:32:09 +0900

    feat: 전체게시글 조회, 상세 게시글 조회 응답값 생성

---
+++ src/main/java/org/nova/backend/board/application/dto/response/PostSummaryResponse.java
@@ -0,0 +1,22 @@
+package org.nova.backend.board.application.dto.response;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class PostSummaryResponse {
+    private UUID id;
+    private String title;
+    private String content;
+    private int viewCount;
+    private int likeCount;
+    private LocalDateTime createdTime;
+    private LocalDateTime modifiedTime;
+    private String authorName;
+    private String authorProfilePhoto;
+}
