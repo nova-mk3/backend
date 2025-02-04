db4d719764ead6730bc7fb39c11c3d6c1b77277a
SinnoLn <sintmtn69@gmail.com>
2025-02-04 14:02:49 +0900

    refactor: member객체 대신 memberId 받게 변경

--- src/main/java/org/nova/backend/board/application/port/in/CommentUseCase.java
+++ src/main/java/org/nova/backend/board/application/port/in/CommentUseCase.java
@@ -4,9 +4,8 @@ import java.util.List;
 import java.util.UUID;
 import org.nova.backend.board.application.dto.request.CommentRequest;
 import org.nova.backend.board.application.dto.response.CommentResponse;
-import org.nova.backend.member.domain.model.entity.Member;
 
 public interface CommentUseCase {
-    CommentResponse addComment(CommentRequest commentRequest, Member member);
+    CommentResponse addComment(UUID postId, CommentRequest commentRequest, UUID memberId);
     List<CommentResponse> getCommentsByPostId(UUID postId);
 }
