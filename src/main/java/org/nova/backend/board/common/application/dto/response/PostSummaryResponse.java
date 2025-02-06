a173846a8b61ba5da325957838f1cb4649c28af2
SinnoLn <sintmtn69@gmail.com>
2025-02-06 15:53:57 +0900

    refactor: 응답값에 카테고리종류 추가

--- src/main/java/org/nova/backend/board/common/application/dto/response/PostSummaryResponse.java
+++ src/main/java/org/nova/backend/board/common/application/dto/response/PostSummaryResponse.java
@@ -5,12 +5,14 @@ import java.util.UUID;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import org.nova.backend.board.common.domain.model.valueobject.PostType;
 
 @Getter
 @NoArgsConstructor
 @AllArgsConstructor
 public class PostSummaryResponse {
     private UUID id;
+    private PostType type;
     private String title;
     private String content;
     private int viewCount;
