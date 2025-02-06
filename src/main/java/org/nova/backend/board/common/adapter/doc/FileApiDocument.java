1549488cbaa1441411d9e92bcb1fbd84d9557bed
SinnoLn <sintmtn69@gmail.com>
2025-02-06 17:18:25 +0900

    feat: 파일 다운로드 스웨거 문서 작성

---
+++ src/main/java/org/nova/backend/board/common/adapter/doc/FileApiDocument.java
@@ -0,0 +1,24 @@
+package org.nova.backend.board.common.adapter.doc;
+
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Target(ElementType.METHOD)
+@Retention(RetentionPolicy.RUNTIME)
+@Tag(name = "File API", description = "파일 업로드 및 다운로드 관련 API")
+public @interface FileApiDocument {
+
+    @Operation(summary = "파일 다운로드", description = "파일 ID를 기반으로 파일을 다운로드합니다.")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "파일 다운로드 성공"),
+            @ApiResponse(responseCode = "404", description = "파일을 찾을 수 없음"),
+            @ApiResponse(responseCode = "500", description = "서버 에러 발생")
+    })
+    @interface DownloadFile {}
+}
