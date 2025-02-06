796a0d77e20565a6a0080d2cc4b6d3e2ad726958
SinnoLn <sintmtn69@gmail.com>
2025-02-06 10:59:04 +0900

    refactor: Java Bean Validation 제거

--- src/main/java/org/nova/backend/board/application/dto/request/CommentRequest.java
+++ src/main/java/org/nova/backend/board/application/dto/request/CommentRequest.java
@@ -1,6 +1,5 @@
 package org.nova.backend.board.application.dto.request;
 
-import jakarta.validation.constraints.NotBlank;
 import java.util.UUID;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
@@ -11,6 +10,5 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 public class CommentRequest {
     private UUID parentCommentId;
-    @NotBlank(message = "댓글 내용은 비어 있을 수 없습니다.")
     private String content;
 }
