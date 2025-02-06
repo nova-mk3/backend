362cf436065f9e6099b4daab14532d0403f9f7fc
SinnoLn <sintmtn69@gmail.com>
2025-02-06 17:32:09 +0900

    feat: 게시글 조회 Security에 경로 추가

--- src/main/java/org/nova/backend/shared/config/SecurityConfig.java
+++ src/main/java/org/nova/backend/shared/config/SecurityConfig.java
@@ -90,7 +90,8 @@ public class SecurityConfig {
                 .requestMatchers(
                         "/api/v1/boards",
                         "/api/v1/boards/{boardId}/posts",
-                        "/api/v1/boards/{boardId}/posts/{postId}"
+                        "/api/v1/boards/{boardId}/posts/{postId}",
+                        "/api/v1/boards/{boardId}/posts/latest"
                 ).permitAll()
 
                 // 로그인한 사용자만 접근 가능한 API (일반 게시글 작성, 수정)
