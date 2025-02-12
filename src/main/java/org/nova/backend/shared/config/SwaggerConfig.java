bd9d596c699044b086af2cba354bf179e1ed0f9c
SinnoLn <sintmtn69@gmail.com>
2025-02-13 02:56:52 +0900

    feat: swagger jwt 토큰받게 수정

--- src/main/java/org/nova/backend/shared/config/SwaggerConfig.java
+++ src/main/java/org/nova/backend/shared/config/SwaggerConfig.java
@@ -2,6 +2,8 @@ package org.nova.backend.shared.config;
 
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -16,7 +18,16 @@ public class SwaggerConfig {
                 .description("CBNU SW 학술 동아리 Nova 홈페이지의 API 명세서")
                 .version("v1");
 
+        SecurityScheme securityScheme = new SecurityScheme()
+                .type(SecurityScheme.Type.HTTP)
+                .scheme("bearer")
+                .bearerFormat("JWT")
+                .name("Authorization");
+
         return new OpenAPI()
-                .info(info);
+                .info(info)
+                .addSecurityItem(new SecurityRequirement().addList("BearerAuth"))
+                .components(new io.swagger.v3.oas.models.Components()
+                        .addSecuritySchemes("BearerAuth", securityScheme));
     }
 }
