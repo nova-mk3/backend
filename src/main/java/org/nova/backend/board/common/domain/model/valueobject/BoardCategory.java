8b880d59567295134aacd7fadf354f9fd7e1bbc2
SinnoLn <sintmtn69@gmail.com>
2025-02-16 21:39:32 +0900

    refactor: 건의게시판 분리

--- src/main/java/org/nova/backend/board/common/domain/model/valueobject/BoardCategory.java
+++ src/main/java/org/nova/backend/board/common/domain/model/valueobject/BoardCategory.java
@@ -5,8 +5,7 @@ import lombok.Getter;
 @Getter
 public enum BoardCategory {
     INTEGRATED("통합 게시판"),
-    CLUB_ARCHIVE("동아리 아카이브"),
-    SUGGESTION("건의 게시판");
+    CLUB_ARCHIVE("동아리 아카이브");
 
     private final String displayName;
 
--- src/main/java/org/nova/backend/board/common/domain/model/valueobject/PostType.java
+++ src/main/java/org/nova/backend/board/common/domain/model/valueobject/PostType.java
@@ -9,8 +9,7 @@ public enum PostType {
     INTRODUCTION("자기소개", BoardCategory.INTEGRATED),
     NOTICE("공지사항", BoardCategory.INTEGRATED),
     EXAM_ARCHIVE("족보 게시판", BoardCategory.CLUB_ARCHIVE),
-    PICTURES("사진 게시판", BoardCategory.CLUB_ARCHIVE),
-    SUGGESTION("건의 게시판", BoardCategory.SUGGESTION);
+    PICTURES("사진 게시판", BoardCategory.CLUB_ARCHIVE);
 
     private final String displayName;
     private final BoardCategory category;
