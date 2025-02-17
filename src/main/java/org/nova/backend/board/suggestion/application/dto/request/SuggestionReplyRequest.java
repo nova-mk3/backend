5677178a66fbdff74e983c45d0f63f58c976ba23
SinnoLn <sintmtn69@gmail.com>
2025-02-17 16:58:57 +0900

    feat: 건의게시판 응답값 생성

---
+++ src/main/java/org/nova/backend/board/suggestion/application/dto/request/SuggestionReplyRequest.java
@@ -0,0 +1,12 @@
+package org.nova.backend.board.suggestion.application.dto.request;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class SuggestionReplyRequest {
+    private String reply;
+}
