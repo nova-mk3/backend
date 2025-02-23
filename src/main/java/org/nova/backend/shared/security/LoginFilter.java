5f81af1b8570ce9fe48b22cefe53b05e1bddf0e1
alswnsp411 <alswnsp411@naver.com>
2025-02-23 23:06:15 +0900

    feat: 쿠키에 로그인 인증 토큰 추가

--- src/main/java/org/nova/backend/shared/security/LoginFilter.java
+++ src/main/java/org/nova/backend/shared/security/LoginFilter.java
@@ -3,6 +3,7 @@ package org.nova.backend.shared.security;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletInputStream;
+import jakarta.servlet.http.Cookie;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import java.io.IOException;
@@ -66,6 +67,14 @@ public class LoginFilter extends UsernamePasswordAuthenticationFilter {
 
         String token = createAuthorizationToken(authentication);
 
+        //쿠키에 토큰 담기
+        Cookie cookie = new Cookie("AUTH_TOKEN", token);
+        cookie.setHttpOnly(true);
+        cookie.setPath("/");
+        cookie.setMaxAge(60 * 60 * 10); // 10시간
+        response.addCookie(cookie);
+
+        // 헤더에 토큰 추가
         response.addHeader("Authorization", "Bearer " + token);
     }
 
