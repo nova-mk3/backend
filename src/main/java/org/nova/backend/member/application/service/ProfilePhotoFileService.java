2d71b65ac7fb2296438cecc22cf725da04802d7a
alswnsp411 <alswnsp411@naver.com>
2025-03-18 01:01:03 +0900

    feat: 회원 프로필 사진 다운로드 기능 추가 - 로그인한 사용자가 다른 사용자의 프로필 사진을 다운로드 할 수 있다. - 기본 이미지는 다운로드 x

--- src/main/java/org/nova/backend/member/application/service/ProfilePhotoFileService.java
+++ src/main/java/org/nova/backend/member/application/service/ProfilePhotoFileService.java
@@ -1,6 +1,11 @@
 package org.nova.backend.member.application.service;
 
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.FileInputStream;
 import java.io.IOException;
+import java.io.InputStream;
+import java.net.URLEncoder;
+import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
@@ -17,9 +22,12 @@ import org.nova.backend.member.domain.model.entity.ProfilePhoto;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.util.StreamUtils;
 import org.springframework.web.multipart.MultipartFile;
 
 @Service
@@ -115,5 +123,41 @@ public class ProfilePhotoFileService {
     public ProfilePhoto findBaseProfilePhoto() {
         return profilePhotoFileRepository.findProfilePhotoByOriginalFilename(baseProfilePhotoName)
                 .orElseThrow(() -> new ProfilePhotoFileDomainException("기본 이미지를 찾을 수 없습니다.", HttpStatus.NOT_FOUND));
+    }
+
+    /**
+     * 프로필 사진 다운로드 처리
+     */
+    private void processProfileDownload(ProfilePhoto profilePhoto, HttpServletResponse response) {
+
+        Path filePath = Paths.get(profilePhoto.getFilePath());
+        if (!Files.exists(filePath)) {
+            throw new ProfilePhotoFileDomainException("프로필 사진이 존재하지 않습니다.", HttpStatus.NOT_FOUND);
+        }
+
+        String encodedFileName = URLEncoder.encode(profilePhoto.getOriginalFilename(), StandardCharsets.UTF_8)
+                .replaceAll("\\+", "%20"); // 공백 문제 해결
+
+        response.setContentType(MediaType.APPLICATION_OCTET_STREAM_VALUE);
+        response.setHeader(HttpHeaders.CONTENT_DISPOSITION,
+                "attachment; filename*=UTF-8''" + encodedFileName);
+
+        try (InputStream inputStream = new FileInputStream(filePath.toFile())) {
+            StreamUtils.copy(inputStream, response.getOutputStream());
+            response.flushBuffer();
+        } catch (IOException e) {
+            throw new FileDomainException("파일 다운로드 중 오류 발생", e);
+        }
+    }
+
+    /**
+     * 프로필 사진 다운로드 (로그인한 사람만 가능)
+     */
+    public void downloadProfilePhoto(
+            UUID profilePhotoId,
+            HttpServletResponse response
+    ) {
+        ProfilePhoto profilePhoto = findProfilePhotoById(profilePhotoId);
+        processProfileDownload(profilePhoto, response);
     }
 }
