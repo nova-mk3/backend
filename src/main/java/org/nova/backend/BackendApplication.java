4eb43bccac565866511e9db9805ea4fdb77cb537
SinnoLn <sintmtn69@gmail.com>
2025-03-18 16:49:16 +0900

    fix: 환경변수 인식되게 수정

--- src/main/java/org/nova/backend/BackendApplication.java
+++ src/main/java/org/nova/backend/BackendApplication.java
@@ -1,5 +1,6 @@
 package org.nova.backend;
 
+import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,8 +8,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class BackendApplication {
 
     public static void main(String[] args) {
+        Dotenv dotenv = Dotenv.load();
+        dotenv.entries().forEach(entry -> System.setProperty(entry.getKey(), entry.getValue()));
+
         SpringApplication.run(BackendApplication.class, args);
     }
-
-
 }
