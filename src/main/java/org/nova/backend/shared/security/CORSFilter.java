a6d160da77b054d6eab965db70ab269cb21a5e4c
Lee Jiny <ok373737@naver.com>
2025-03-20 09:10:26 +0900

    Dev에서 머리 (#76)
    * feat: access token 만료 여부 검증 api 추가
    
    * feat: 마이페이지 api 추가
    - 회원정보 수정 api
    - 회원 정보 조회 api
    - 이메일 조회 api 추가
    - 프로필 사진 업로드 api 추가
    - 프로필 사진 변경 api 수정
    - 프로필 사진 삭제 api 추가
    - 회원 프로필 조회 시 졸업생 정보가 없으면 빈 값 응답
    
    * feat: restful하게 api 수정..
    
    * feat: 마이페이지에서 학년, 학기 정보 string 형으로 반환
    - ex. 기존 : 1, 1,  -> 변경 후 : 1학년, 1학기
    
    * feat: 회원 간단 프로필 조회 api 추가
    
    * Fix: 사진게시판 SpringSecurity 추가, 한글 파일 다운 수정, 젠킨스 파이프라인 수정 (#65)
    
    * fix: 젠킨스 파이프라인 수정
    
    * docs: README.md 추가
    
    * fix: 사진 게시판 경로 spring security 추가
    
    * fix: 한글파일 다운 안되던거 수정
    
    * fix: 엔진엑스 삭제 (#67)
    
    * fix: pendingMember에서 profilePhoto cascade ALL 설정 삭제
    
    * feat: 회원 프로필 사진 다운로드 기능 추가
    - 로그인한 사용자가 다른 사용자의 프로필 사진을 다운로드 할 수 있다.
    - 기본 이미지는 다운로드 x
    
    * Fix: 환경변수 세팅, spring-app health check (#69)
    
    * fix: 엔진엑스 삭제
    
    * fix: 필요없는 엔진엑스 설정 삭제
    
    * fix: 환경변수 인식되게 수정
    
    * refactor: 리팩토링
    
    * fix: spring-app health check 수정
    
    * Fix: 도커 볼륨 추가 (#73)
    
    * fix: mysql볼륨설정
    
    * merge --squash dev
    
    * refactor: CORS설정 추가 (#75)
    
    * fix: mysql볼륨설정
    
    * merge --squash dev
    
    * refactor: CORS 설정 도메인 추가
    
    ---------
    
    Co-authored-by: alswnsp411 <alswnsp411@naver.com>
    Co-authored-by: Minju Kim <74577811+alswnsp411@users.noreply.github.com>

--- src/main/java/org/nova/backend/shared/security/CORSFilter.java
+++ src/main/java/org/nova/backend/shared/security/CORSFilter.java
@@ -28,7 +28,7 @@ public class CORSFilter implements Filter, jakarta.servlet.Filter {
         HttpServletResponse response = (HttpServletResponse) res;
 
         response.setHeader("Access-Control-Allow-Origin",
-                "http://localhost:8080, http://localhost:3000, http://localhost:3001");
+                "https://jinybook.site, http://localhost:8080, http://localhost:3000, http://localhost:3001");
         response.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
         response.setHeader("Access-Control-Allow-Headers", "Content-Type");
         response.setHeader("Access-Control-Allow-Credentials", "true");
