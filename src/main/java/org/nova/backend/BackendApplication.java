87e74c81ab16ecea2f208f201a9230060224ec41
SinnoLn <sintmtn69@gmail.com>
2025-03-21 16:26:39 +0900

    fix: .env파일 루트 경로에서 인식하도록 변경

--- src/main/java/org/nova/backend/BackendApplication.java
+++ src/main/java/org/nova/backend/BackendApplication.java
@@ -8,7 +8,11 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class BackendApplication {
 
     public static void main(String[] args) {
-        Dotenv dotenv = Dotenv.load();
+        Dotenv dotenv = Dotenv.configure()
+                .directory(System.getProperty("user.dir"))
+                .filename(".env")
+                .load();
+
         dotenv.entries().forEach(entry -> System.setProperty(entry.getKey(), entry.getValue()));
 
         SpringApplication.run(BackendApplication.class, args);
