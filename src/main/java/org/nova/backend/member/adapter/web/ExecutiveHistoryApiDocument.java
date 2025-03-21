208fba1b7b3e845432600bc60ca26f12fde1e433
SinnoLn <sintmtn69@gmail.com>
2025-03-21 14:27:59 +0900

    merge -squash main

--- src/main/java/org/nova/backend/member/adapter/web/ExecutiveHistoryApiDocument.java
+++ src/main/java/org/nova/backend/member/adapter/web/ExecutiveHistoryApiDocument.java
@@ -16,50 +16,69 @@ public @interface ExecutiveHistoryApiDocument {
 
     @Operation(summary = "모든 연도 조회", description = "연도 리스트를 조회합니다.")
     @ApiResponses({
-            @ApiResponse(responseCode = "200", description = "리스트 조회 성공",
-                    content = @Content(mediaType = "application/json")),
-            @ApiResponse(responseCode = "500", description = "서버 오류",
-                    content = @Content(mediaType = "application/json"))
+            @ApiResponse(responseCode = "200", description = "리스트 조회 성공"),
+            @ApiResponse(responseCode = "403", description = "인증되지 않은 접근입니다."),
+            @ApiResponse(responseCode = "500", description = "서버 오류")
     })
     @Target(ElementType.METHOD)
     @Retention(RetentionPolicy.RUNTIME)
     @interface GetYearListApiDoc {
     }
 
+    @Operation(summary = "연도 추가", description = "현재 연도 리스트에서 가장 최근 연도를 찾아 +1 한 값을 추가합니다. 이 연도에 관리자가 포함됩니다.")
+    @ApiResponses({
+            @ApiResponse(responseCode = "200", description = "연도 추가 성공"),
+            @ApiResponse(responseCode = "403", description = "인증되지 않은 접근입니다."),
+            @ApiResponse(responseCode = "500", description = "서버 오류")
+    })
+    @Target(ElementType.METHOD)
+    @Retention(RetentionPolicy.RUNTIME)
+    @interface AddYearApiDoc {
+    }
+
+    @Operation(summary = "임원 권한 변경", description = "임원에게 노바 홈페이지 관리 권한을 부여합니다.")
+    @ApiResponses({
+            @ApiResponse(responseCode = "200", description = "임원 권한 변경 성공"),
+            @ApiResponse(responseCode = "403", description = "인증되지 않은 접근입니다."),
+            @ApiResponse(responseCode = "404", description = "해당 임원을 찾을 수 없습니다."),
+            @ApiResponse(responseCode = "409", description = "해당 임원에게 권한을 부여할 수 없습니다."),
+            @ApiResponse(responseCode = "500", description = "서버 오류")
+    })
+    @Target(ElementType.METHOD)
+    @Retention(RetentionPolicy.RUNTIME)
+    @interface UpdateExecutivesRoleApiDoc {
+    }
+
     @Operation(summary = "특정 연도의 임원 이력 조회", description = "특정 연도의 임원 이력을 반환합니다.")
     @ApiResponses({
-            @ApiResponse(responseCode = "200", description = "임원 이력 조회 성공",
-                    content = @Content(mediaType = "application/json")),
-            @ApiResponse(responseCode = "400", description = "잘못된 요청 데이터",
-                    content = @Content(mediaType = "application/json")),
-            @ApiResponse(responseCode = "500", description = "서버 오류",
-                    content = @Content(mediaType = "application/json"))
+            @ApiResponse(responseCode = "200", description = "임원 이력 조회 성공"),
+            @ApiResponse(responseCode = "400", description = "잘못된 요청 데이터"),
+            @ApiResponse(responseCode = "403", description = "인증되지 않은 접근입니다."),
+            @ApiResponse(responseCode = "500", description = "서버 오류")
     })
     @Target(ElementType.METHOD)
     @Retention(RetentionPolicy.RUNTIME)
     @interface GetExecutiveHistoryByYearApiDoc {
     }
 
-    @Operation(summary = "임원 추가", description = "요청에 대한 새로운 임원 이력 추가")
+    @Operation(summary = "임원 이력 추가", description = "요청에 대한 새로운 임원 이력 추가")
     @ApiResponses({
-            @ApiResponse(responseCode = "200", description = "임원 이력 추가 완료",
-                    content = @Content(mediaType = "application/json")),
-            @ApiResponse(responseCode = "400", description = "잘못된 요청 데이터",
-                    content = @Content(mediaType = "application/json")),
-            @ApiResponse(responseCode = "500", description = "서버 오류",
-                    content = @Content(mediaType = "application/json"))
+            @ApiResponse(responseCode = "200", description = "임원 이력 추가 완료"),
+            @ApiResponse(responseCode = "400", description = "잘못된 요청 데이터"),
+            @ApiResponse(responseCode = "403", description = "인증되지 않은 접근입니다."),
+            @ApiResponse(responseCode = "409", description = "연도 추가 후 임원 이력을 추가해주세요."),
+            @ApiResponse(responseCode = "500", description = "서버 오류")
     })
     @Target(ElementType.METHOD)
     @Retention(RetentionPolicy.RUNTIME)
     @interface AddExecutiveHistoryApiDoc {
     }
 
-    @Operation(summary = "임원 삭제", description = "요청에 대한 임원 이력 삭제")
+    @Operation(summary = "임원 이력 삭제", description = "요청에 대한 임원 이력 삭제")
     @ApiResponses({
-            @ApiResponse(responseCode = "200", description = "임원 이력 삭제 완료",
-                    content = @Content(mediaType = "application/json")),
-            @ApiResponse(responseCode = "400", description = "잘못된 요청 데이터",
-                    content = @Content(mediaType = "application/json")),
+            @ApiResponse(responseCode = "200", description = "임원 이력 삭제 완료"),
+            @ApiResponse(responseCode = "400", description = "잘못된 요청 데이터"),
+            @ApiResponse(responseCode = "403", description = "인증되지 않은 접근입니다."),
             @ApiResponse(responseCode = "404", content = @Content(mediaType = "application/json",
                     examples = {@ExampleObject(name = "ExecutiveHistory 조회 실패",
                             value = "{\"code\": 404, \"message\": \"executive history not found. 20202020\"}"),
@@ -75,10 +94,9 @@ public @interface ExecutiveHistoryApiDocument {
 
     @Operation(summary = "모든 회원 목록 불러오기", description = "임원 추가를 위해 회원 목록을 불러옵니다.")
     @ApiResponses({
-            @ApiResponse(responseCode = "200", description = "모든 회원 목록 반환 완료",
-                    content = @Content(mediaType = "application/json")),
-            @ApiResponse(responseCode = "500", description = "서버 오류",
-                    content = @Content(mediaType = "application/json"))
+            @ApiResponse(responseCode = "200", description = "모든 회원 목록 반환 완료"),
+            @ApiResponse(responseCode = "403", description = "인증되지 않은 접근입니다."),
+            @ApiResponse(responseCode = "500", description = "서버 오류")
     })
     @Target(ElementType.METHOD)
     @Retention(RetentionPolicy.RUNTIME)
--- src/main/java/org/nova/backend/member/application/mapper/ExecutiveHistoryMapper.java
+++ src/main/java/org/nova/backend/member/application/mapper/ExecutiveHistoryMapper.java
@@ -1,14 +1,19 @@
 package org.nova.backend.member.application.mapper;
 
 import java.util.UUID;
+import lombok.RequiredArgsConstructor;
 import org.nova.backend.member.application.dto.request.AddExecutiveHistoryRequest;
 import org.nova.backend.member.application.dto.response.ExecutiveHistoryResponse;
+import org.nova.backend.member.application.dto.response.ProfilePhotoResponse;
 import org.nova.backend.member.domain.model.entity.ExecutiveHistory;
 import org.nova.backend.member.domain.model.entity.Member;
 import org.springframework.stereotype.Component;
 
 @Component
+@RequiredArgsConstructor
 public class ExecutiveHistoryMapper {
+
+    private final MemberProfilePhotoMapper memberProfilePhotoMapper;
 
     public ExecutiveHistory toEntity(AddExecutiveHistoryRequest request, Member member) {
         String name = request.getName();
@@ -25,12 +30,18 @@ public class ExecutiveHistoryMapper {
     }
 
     public ExecutiveHistoryResponse toResponse(ExecutiveHistory executiveHistory) {
+        ProfilePhotoResponse profilePhotoResponse = executiveHistory.getMember() != null ?
+                memberProfilePhotoMapper.toResponse(executiveHistory.getMember().getProfilePhoto())
+                : memberProfilePhotoMapper.toResponse(null);
+
         return new ExecutiveHistoryResponse(
                 executiveHistory.getId(),
                 executiveHistory.getYear(),
                 executiveHistory.getRole(),
                 executiveHistory.getName(),
-                executiveHistory.getMember() != null ? executiveHistory.getMember().getId() : null
+                executiveHistory.getMember() != null ? executiveHistory.getMember().getId() : null,
+                profilePhotoResponse,
+                executiveHistory.getMember() != null ? executiveHistory.getMember().getPhone() : null
         );
     }
 
