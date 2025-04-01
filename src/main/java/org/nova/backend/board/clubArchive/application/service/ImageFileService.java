f2e40b0f50816b53555aec76799a047ea4096728
SinnoLn <sintmtn69@gmail.com>
2025-04-02 02:31:28 +0900

    feat: update image URL generation logic to map protected files to public folder

--- src/main/java/org/nova/backend/board/clubArchive/application/service/ImageFileService.java
+++ src/main/java/org/nova/backend/board/clubArchive/application/service/ImageFileService.java
@@ -12,6 +12,7 @@ import org.nova.backend.board.clubArchive.application.dto.response.ImageResponse
 import org.nova.backend.board.clubArchive.domain.exception.PictureDomainException;
 import org.nova.backend.board.common.application.port.in.FileUseCase;
 import org.nova.backend.board.common.domain.model.entity.File;
+import org.nova.backend.shared.constants.FilePathConstants;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
@@ -32,7 +33,8 @@ public class ImageFileService {
      */
     public ImageResponse createImageResponse(File file) {
         boolean isImage = isImageFile(file.getOriginalFilename());
-        int width = 0, height = 0;
+        int width = 0;
+        int height = 0;
 
         if (isImage) {
             try {
@@ -85,12 +87,13 @@ public class ImageFileService {
      */
     public String convertFilePathToUrl(String filePath) {
         Path basePath = Paths.get(baseFileStoragePath);
+        Path protectedPath = basePath.resolve(FilePathConstants.PROTECTED_FOLDER);
         Path absolutePath = Paths.get(filePath);
 
-        if (absolutePath.startsWith(basePath)) {
-            Path relativePath = basePath.relativize(absolutePath);
-            return "/files/" + relativePath.toString().replace("\\", "/");
+        if (absolutePath.startsWith(protectedPath)) {
+            Path fileName = absolutePath.getFileName();
+            return "/files/" + FilePathConstants.PUBLIC_FOLDER + "/" + fileName.toString();
         }
-        return "/files/" + absolutePath.getFileName().toString().replace("\\", "/");
+        return "/files/" + absolutePath.getFileName().toString();
     }
 }
