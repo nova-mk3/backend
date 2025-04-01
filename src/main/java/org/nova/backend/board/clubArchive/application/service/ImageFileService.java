a07be425c76e977e4616f7eb529f0ab2168b91f2
Lee Jiny <ok373737@naver.com>
2025-04-02 04:53:31 +0900

    fix: modify return url (#119)
    * fix: add CascadeType.ALL on postLike
    
    * fix:separate .yml file
    
    * fix: modify return url
    
    * fix: modify return url
    
    * fix: modify return url

--- src/main/java/org/nova/backend/board/clubArchive/application/service/ImageFileService.java
+++ src/main/java/org/nova/backend/board/clubArchive/application/service/ImageFileService.java
@@ -86,14 +86,7 @@ public class ImageFileService {
      * 파일 시스템 경로를 웹 URL로 변환
      */
     public String convertFilePathToUrl(String filePath) {
-        Path basePath = Paths.get(baseFileStoragePath);
-        Path protectedPath = basePath.resolve(FilePathConstants.PROTECTED_FOLDER);
         Path absolutePath = Paths.get(filePath);
-
-        if (absolutePath.startsWith(protectedPath)) {
-            Path fileName = absolutePath.getFileName();
-            return "/files/public/" + FilePathConstants.PUBLIC_FOLDER + "/" + fileName.toString();
-        }
-        return "/files/public/" + absolutePath.getFileName().toString();
+        return FilePathConstants.PUBLIC_FILE_URL_PREFIX + absolutePath.getFileName().toString();
     }
 }
