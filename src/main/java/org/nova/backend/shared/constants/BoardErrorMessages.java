e9139cca561ce5a939f909bd06d976e521f20d14
SinnoLn <sintmtn69@gmail.com>
2025-04-08 21:11:43 +0900

    feat: add error messages

--- src/main/java/org/nova/backend/shared/constants/BoardErrorMessages.java
+++ src/main/java/org/nova/backend/shared/constants/BoardErrorMessages.java
@@ -4,7 +4,7 @@ public class BoardErrorMessages {
     private BoardErrorMessages() {
         throw new UnsupportedOperationException("Utility class");
     }
-    
+
     public static final String POST_NOT_FOUND = "게시글을 찾을 수 없습니다.";
     public static final String INVALID_BOARD_ID = "잘못된 게시판 ID입니다.";
     public static final String NO_AUTHORITY = "게시글 수정 권한이 없습니다.";
