44058fe1fb50d56fd031d065ee901b5e70b44233
SinnoLn <sintmtn69@gmail.com>
2025-05-01 00:15:15 +0900

    chore: remove unused imports in BasePostMapper

--- src/main/java/org/nova/backend/board/common/application/mapper/BasePostMapper.java
+++ src/main/java/org/nova/backend/board/common/application/mapper/BasePostMapper.java
@@ -11,17 +11,12 @@ import org.nova.backend.board.common.application.dto.response.BasePostSummaryRes
 import org.nova.backend.board.common.application.dto.response.FileResponse;
 import org.nova.backend.board.common.domain.model.entity.Board;
 import org.nova.backend.board.common.domain.model.entity.Post;
-import org.nova.backend.member.application.dto.response.ProfilePhotoResponse;
-import org.nova.backend.member.application.mapper.MemberProfilePhotoMapper;
 import org.nova.backend.member.domain.model.entity.Member;
-import org.nova.backend.member.domain.model.entity.ProfilePhoto;
 import org.springframework.stereotype.Component;
 
 @Component
 @AllArgsConstructor
 public class BasePostMapper {
-
-    private MemberProfilePhotoMapper profilePhotoMapper;
 
     public Post toEntity(
             BasePostRequest request,
