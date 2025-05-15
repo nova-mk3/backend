dbd0ac81fe1d5fcd091e6fd1074349185168376a
SinnoLn <sintmtn69@gmail.com>
2025-05-16 00:24:37 +0900

    refactor: remove redundant @GeneratedValue on UUID primary key in Notification entity

--- src/main/java/org/nova/backend/notification/domain/model/entity/Notification.java
+++ src/main/java/org/nova/backend/notification/domain/model/entity/Notification.java
@@ -5,8 +5,6 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.EnumType;
 import jakarta.persistence.Enumerated;
 import jakarta.persistence.FetchType;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
@@ -28,7 +26,6 @@ import org.nova.backend.notification.domain.model.entity.valueobject.EventType;
 public class Notification {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
     private UUID id;
 
     @ManyToOne(fetch = FetchType.LAZY)
