adb50b146e427c48cfc5e98f6cd5365db93e48d4
Lee Jiny <ok373737@naver.com>
2025-05-21 15:02:08 +0900

    feat: 건의게시판 알림의 targetType을 SUGGESTION으로 지정 및 null 데이터 마이그레이션 (#145)

--- src/main/java/org/nova/backend/board/common/domain/model/valueobject/PostType.java
+++ src/main/java/org/nova/backend/board/common/domain/model/valueobject/PostType.java
@@ -10,7 +10,8 @@ public enum PostType {
     INTRODUCTION("자기소개", BoardCategory.INTEGRATED),
     NOTICE("공지사항", BoardCategory.INTEGRATED),
     EXAM_ARCHIVE("족보 게시판", BoardCategory.CLUB_ARCHIVE),
-    PICTURES("사진 게시판", BoardCategory.CLUB_ARCHIVE);
+    PICTURES("사진 게시판", BoardCategory.CLUB_ARCHIVE),
+    SUGGESTION("건의 게시판", null);
 
     private final String displayName;
     private final BoardCategory category;
